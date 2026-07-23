7807427d32e036d1eae00784613493a62bcb77b6
Ake Sharma <113997267+Akesharma009@users.noreply.github.com>
2026-07-23 17:56:07 +0545

    import os

--- app/auth.py
+++ app/auth.py
@@ -1,6 +1,7 @@
+import os
 from datetime import datetime
 
-from flask import Blueprint, render_template, redirect, url_for, request, flash, session
+from flask import Blueprint, render_template, redirect, url_for, request, flash, session, current_app
 from flask_login import login_user, logout_user, login_required, current_user
 
 from app.models import db, User, Teacher, Student
